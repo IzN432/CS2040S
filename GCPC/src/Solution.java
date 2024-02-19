d2c30c46b82db60d0686001a2f7e59eee4d44411
IzN432 <isaac.ng432@gmail.com>
2024-02-20 00:22:49 +0800

    gcpc start;

---
+++ GCPC/src/Solution.java
@@ -0,0 +1,35 @@
+
+public class Solution {
+    // TODO: Include your data structures here
+
+    private static class RankTree {
+
+        private static class Node {
+            private int weight;
+        }
+
+        private void insert(int value) {
+
+        }
+
+        private Node find(int rank) {
+            return null;
+        }
+
+        private Enum
+        private void rotate() {
+
+        }
+
+    }
+
+    public Solution(int numTeams) {
+        // TODO: Construct/Initialise your data structures here
+    }
+
+    public int update(int team, long newPenalty){
+        // TODO: Implement your update function here
+        return 0;
+    }
+
+}
--- TestingCode/src/Main.java
+++ TestingCode/src/Main.java
@@ -1,15 +1,76 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        test(1000000, 10);
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    static Random random = new Random(1);
+    private static void test(int iterations, int bound) {
+        for (int i = 0; i < iterations; i++) {
+            int height = random.nextInt(100) + 1;
+            int width = random.nextInt(100) + 1;
+            int x = random.nextInt(width);
+            int y = random.nextInt(height);
+
+            int sol1 = mathSolution(x, y, width, height);
+            int sol2 = yinSolution(x, y, width, height);
+            if (sol1 != sol2)
+                System.out.printf("H: %d, W: %d, x: %d, y: %d\n%d vs %d\n", height, width, x, y, sol1, sol2);
+
+
         }
+
+    }
+
+    private static int mathSolution(int x, int y, int width, int height) {
+        int topBound = height - y - 1;
+        int bottomBound = y;
+        int leftBound = x;
+        int rightBound = width - x - 1;
+
+        int[] arr = new int[] {topBound, bottomBound, leftBound, rightBound};
+        Arrays.sort(arr);
+
+        int lowestBound = arr[0];
+
+        // find the number of outer layer steps
+        int outerLayers = width * height - (width - 2 * lowestBound) * (height - 2 * lowestBound);
+        x = x - lowestBound;
+        y = y - lowestBound;
+
+        int newWidth = width - lowestBound * 2;
+        int newHeight = height - lowestBound * 2;
+        if (y == 0) return outerLayers + x;
+        if (x == newWidth - 1) return outerLayers + newWidth - 1 + y;
+        if (y == newHeight - 1) return outerLayers + newWidth - 1 + newHeight - 1 + newWidth - x - 1;
+        if (x == 0) return outerLayers + newWidth - 1 + newHeight - 1 + newWidth - 1 + newHeight - y - 1;
+
+        return 0;
+    }
+
+    private static int solution(int x, int y, int width, int height) {
+        if (y == 0 && x == 0) return 0;
+        for (int i = 0; i < width; i++) {
+            x--;
+            if (x == 0 && y == 0) return i + 1;
+        }
+
+        return width + solution(y - 1, - x - 1, height - 1, width);
+    }
+
+    private static int yinSolution(int x, int y, int width, int height) {
+        return yinHelper(x + 1, y, width, height) - 1;
+    }
+
+    private static int yinHelper(int x, int y, int width, int height) {
+        for (int i = 0; i < width; i++) {
+            x -= 1;
+            if (x == 0 && y == 0) {
+                return i + 1;
+            }
+        }
+        return width + yinHelper(y, -x, height - 1, width);
     }
 }
